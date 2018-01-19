18c9d03892bdfb090198d776dcaecb707ad2d6b6
Dustin Frank <dustintstrange@gmail.com>
2018-01-18 19:30:25 -0800

    started on routes in lift-oversController

--- controller/lift-overController.js
+++ controller/lift-overController.js
@@ -10,6 +10,11 @@ router.get("/", function(req, res){
     res.render("index")
 });
 
+//login or register request
+// router.post("/api/lift-overs",(req, res){
+//     if()
+// });
+
 
 module.exports = router;
 

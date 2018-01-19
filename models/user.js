b4a85e7311766d88bb4558dacd53320e948bbaab
Alex Green <alex@devfarms.com>
2018-01-18 19:31:47 -0800

    Added additional sequelize models

--- models/user.js
+++ models/user.js
@@ -1,27 +1,31 @@
 // Dependicies
 module.exports = function(sequelize, DataTypes){
 
-// Create a "User" model
-var User = sequelize.define('user_tbl', {
-    user_id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    user_first_name: {
-        type: DataTypes.STRING
-    },
-    user_last_name: {
-        type: DataTypes.STRING
-    },
-    user_email: {
-        type: DataTypes.STRING
-    }
-}, {
-  timestamps: false
-});
-    return User;
-
-}
-
-
+    // Create a "User" model
+    var User = sequelize.define('user_tbl', {
+        user_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        user_first_name: {
+            type: DataTypes.STRING
+        },
+        user_last_name: {
+            type: DataTypes.STRING
+        },
+        user_phone: {
+            type: DataTypes.STRING
+        },
+        user_email: {
+            type: DataTypes.STRING
+        },
+        user_type: {
+            type: DataTypes.STRING
+        }
+    }, {
+      timestamps: false
+    });
+        return User;
+    
+    }    

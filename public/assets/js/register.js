f672d56867a7f94f55586cae067905dbeefd0a79
elmarc <elmarc@wsip-64-58-160-81.oc.oc.cox.net>
2018-01-24 18:00:21 -0800

    reduced number of login pages and delteted useless routes

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
     $("#register-form").on("submit", function(event) {
         event.preventDefault()
+        var userFirstName = $("#first_name").val().trim();
+        var userLastName = $("#last_name").val().trim();
+        var userType = $("#user_type").val().trim();
         var userEmail = $("#user_email").val().trim();
         var userPassword = $("#user_password").val().trim();
-        var userType = $("#user_type").val().trim();
 
         $.ajax({
                 method: "POST",
                 dataType: "json",
                 url: "/api/user/create",
                 data: {
-                    first_name: userEmail,
-                    user_password: userPassword,
-                    user_type: userType
+                    first_name: userFirstName,
+                    last_name: userLastName,
+                    user_type: userType,
+                    user_email: userEmail,
+                    user_password: userPassword
                 }
             })
             .done(function(user) {
                 console.log(user);
-                location.href = "/driver-reg";
+                location.href = "/user/driver";
             })
             .fail(function(xhr, text, error) {
                 console.log(error);

578f9f9a1b88a2f89b4c9ad68ce20d17aae9928e
Dustin Frank <dustintstrange@gmail.com>
2018-01-27 11:36:49 -0800

    added vars to register.js for first and last name

--- public/assets/js/register.js
+++ public/assets/js/register.js
@@ -4,9 +4,13 @@ $(document).ready(function () {
         var email = $("#login_email").val().trim();
         var password = $("#login_password").val().trim();
         var type = $('#login_type').find(":selected").text();
+        var firstName = $("#user_first_name").val().trim();
+        var lastName = $("#user_last_name").val().trim();
+        
+
         //console.log(email, password);
 
-        axios.post("/auth/register", { email: email, password: password, user_type: type })
+        axios.post("/auth/register", { email: email, password: password, user_type: type, user_first_name: firstName, user_last_name:lastName })
             .then(function (response) {
                 console.log(response);
                 location.assign("/login");
